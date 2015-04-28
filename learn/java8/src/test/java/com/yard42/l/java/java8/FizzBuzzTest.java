20e0699b02273c03987571c707da6fbd487a2cfc
esedin <esedin>
2015-04-28 16:41:21 +0300

    FizzBuzz

---
+++ learn/java8/src/test/java/com/yard42/l/java/java8/FizzBuzzTest.java
@@ -0,0 +1,33 @@
+package com.yard42.l.java.java8;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class FizzBuzzTest
+{
+   @Test
+   public void testWithNumberIsDividableBy3()
+   {
+      Assert.assertEquals("Fizz", FizzBuzz.fizzBuzz(3));
+   }
+
+   @Test
+   public void testWithNumberIsDividableBy5()
+   {
+      Assert.assertEquals("Buzz", FizzBuzz.fizzBuzz(5));
+   }
+
+   @Test
+   public void testWithNumberIsDividableBy15()
+   {
+      Assert.assertEquals("FizzBuzz", FizzBuzz.fizzBuzz(15));
+      Assert.assertEquals("FizzBuzz", FizzBuzz.fizzBuzz(45));
+   }
+
+   @Test
+   public void testOtherNumbers()
+   {
+      Assert.assertEquals("1", FizzBuzz.fizzBuzz(1));
+      Assert.assertEquals("7", FizzBuzz.fizzBuzz(7));
+   }
+}
