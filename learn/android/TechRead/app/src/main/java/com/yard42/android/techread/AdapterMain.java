9bccb7254195b4be020ea61d6344df6d35ab8d99
esedin <esedin>
2015-09-01 07:48:11 +0300

    Added new android and chrome project
    1

---
+++ learn/android/TechRead/app/src/main/java/com/yard42/android/techread/AdapterMain.java
@@ -0,0 +1,10 @@
+package com.yard42.android.techread;
+
+import android.support.v7.widget.RecyclerView;
+
+/**
+ * Created by Ievgen on 5/17/2015.
+ */
+public class AdapterMain implements RecyclerView.Adapter<AdapterMain.ViewHolder>{
+    
+}
---
+++ learn/chromedev/OneVizitThis/main.js
@@ -0,0 +1,6 @@
+window.onload = function() {
+  //chrome.tabs.create({'url' : chrome.extension.getURL('index.html')});
+  
+  //document.querySelector('#greeting').innerText =
+  //  'Hello, World! It is ' + new Date();
+};
