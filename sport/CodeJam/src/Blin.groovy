1e27fcea64a4991e5ca0f0778325010f71397439
esedin <esedin>
2016-04-12 11:35:48 +0300

    CodeJam 2-nd task
    CodeJam 2-nd task

---
+++ sport/CodeJam/src/Blin.groovy
@@ -0,0 +1,19 @@
+def caseNumber = 0;
+
+def fileName = '..\\B-small-practice.in';
+def input = new FileInputStream(fileName)
+def reader = new BufferedReader(new InputStreamReader(input))
+
+//Skip first redundant line
+def count = reader.readLine().toInteger();
+
+reader.eachLine {
+   it += '+'
+   def flipCount = 0;
+
+   for (int i = 0; i < it.size() - 1; i++)
+      if (it[i] != it[i + 1]) n++;
+
+   println "Case #" + ++caseNumber + ": " + n
+}
+
